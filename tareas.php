1f59e3889664ff521c493838046e720f06322691
pierdlcr2006 <185874988+pierdlcr2006@users.noreply.github.com.>
2025-04-05 18:30:01 -0500

    tareas.php

--- tareas.php
+++ tareas.php
@@ -1,5 +1,5 @@
 <?php
-require '../funciones.php';
+require 'funciones.php';
 
 $auth = islog();
 if  (!$auth){
@@ -7,17 +7,38 @@ if  (!$auth){
 }
 $tareas = $_SESSION['tareas'] ?? false;
 $val = $_GET['val'] ?? null;
-
-?>
-<?php
-    if ($val == 1){
-        echo 'TAREA AGREGADA';
-    }
-?>
-<a href="cerrar.php">Cerrar Sesión</a>
-<a href="crear.php">Crear Tarea</a>
-<?php
-if (!$tareas){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $indice = filter_var($_POST['indice'],FILTER_SANITIZE_NUMBER_INT);
+    unset($tareas[$indice]);
+    $_SESSION['tareas'] = $tareas;
 
 }
 ?>
+<?php
+    if ($val == 1):?>
+        <h1>TAREA REGISTRADA</h1>
+<?php endif; ?>
+<a href="cerrar.php">Cerrar Sesión</a>
+<a href="crear.php">Crear Tarea</a>
+
+<h1>TAREAS</h1>
+<table>
+    <tr>
+        <th>Tarea</th>
+    </tr>
+    <?php if ($tareas):
+    foreach ($tareas as $i => $tarea): ?>
+        <tr>
+            <td>
+                <?php echo $tarea; ?>
+                <a href="actualizar.php?id=<?php echo $i ?>">Actualizar</a>
+                <form method="post">
+                    <input type="submit" value="Eliminar"/>
+                    <input type="hidden" name="indice" value="<?php echo $i ?> >"/>
+                </form>
+            </td>
+
+        </tr>
+    <?php endforeach;
+    endif ?>
+</table>
